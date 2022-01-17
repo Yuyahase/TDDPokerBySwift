adb17b09ccc3a605a107645e35820c63cc6184da
haseyuy <haseyuy@gmail.com>
2022-01-17 18:29:57 +0900

    Initial Commit

---
+++ TDDPokerBySwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDDPokerBySwift
+//
+//  Created by 長谷侑弥 on 2022/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TDDPokerBySwift/TDDPokerBySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDPokerBySwiftApp.swift
+//  TDDPokerBySwift
+//
+//  Created by 長谷侑弥 on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct TDDPokerBySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
